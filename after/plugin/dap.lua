f85e64782fb20895a0e1a0e76bb9a95d1e83e1ea
pgxtips <brandongill123@gmail.com>
2025-03-13 09:50:01 +0000

    feat: added debugging for js

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -12,9 +12,28 @@ dap.adapters.codelldb = {
     type = 'server',
     port = "${port}",
     executable = {
+        command = os.getenv("HOME") .. "/.local/share/nvim/mason/bin/debugpy-adapter",
+        args = {"--port", "${port}"},
+    }
+}
+
+dap.adapters.debugpy = {
+    type = 'server',
+    port = "${port}",
+    executable = {
         command = os.getenv("HOME") .. "/.local/share/nvim/mason/bin/codelldb",
         args = {"--port", "${port}"},
     }
+}
+
+dap.adapters["pwa-node"] = {
+  type = "server",
+  host = "localhost",
+  port = "${port}",
+  executable = {
+    command = "node",
+    args = {os.getenv("HOME") .. "/.local/share/nvim/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js", "${port}"},
+  }
 }
 
 -- adapter configurations
@@ -46,6 +65,42 @@ dap.configurations.c = {
 
 dap.configurations.cpp = dap.configurations.c
 dap.configurations.rust = dap.configurations.c
+
+dap.configurations.python = {
+  {
+    -- The first three options are required by nvim-dap
+    type = 'python'; -- the type here established the link to the adapter definition: `dap.adapters.python`
+    request = 'launch';
+    name = "Launch file";
+
+    -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+    program = "${file}"; -- This configuration will launch the current file if used.
+    pythonPath = function()
+      -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+      -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+      -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+      local cwd = vim.fn.getcwd()
+      if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+        return cwd .. '/venv/bin/python'
+      elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+        return cwd .. '/.venv/bin/python'
+      else
+        return '/usr/bin/python'
+      end
+    end;
+  },
+}
+
+dap.configurations.javascript = {
+  {
+    type = "pwa-node",
+    request = "launch",
+    name = "Launch file",
+    program = "${file}",
+    cwd = "${workspaceFolder}",
+  },
+}
 
 -- setup dap ui
 dapui.setup({})
